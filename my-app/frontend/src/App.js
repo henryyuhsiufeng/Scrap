62c7f3c833f48594f5cde00239eb0998a230a8d2
Henry Feng <henryfeng12345@gmail.com>
2020-03-04 17:03:34 -0600

    set up

--- my-app/frontend/src/App.js
+++ my-app/frontend/src/App.js
@@ -1,17 +1,25 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-
-//components
-import TopNavBar from './components/TopNavBar/TopNavBar';
-import Scrape from './components/scrape/scrape';
 
 function App() {
   return (
-    <div>
-      <TopNavBar/>
-      <Scrape/>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-    
   );
 }
 
