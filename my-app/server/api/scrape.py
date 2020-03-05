f3a6f48e29f6b1cf3852162b768551a4655a6dcb
Henry Feng <henryfeng12345@gmail.com>
2020-03-05 09:32:24 -0600

    add bs4 and requests module

---
+++ my-app/server/api/scrape.py
@@ -0,0 +1,18 @@
+## Installed bs4 and requests
+
+## 
+import requests
+from bs4 import BeautifulSoup
+
+# using requests module get the webpage we will be scraping
+result = requests.get('https://apps.cs.utexas.edu/calendar/')
+
+# store content of webpage
+src = result.content
+
+# make sure webpage is accessible, should get 200 
+print(result.status_code)
+
+def scrapeUTCS():
+    food_UTCS = ['pizza','soda','taco']
+    return food_UTCS
--- my-app/server/api/views.py
+++ my-app/server/api/views.py
@@ -1,9 +1,14 @@
 from flask import Blueprint, jsonify
+from .scrape import scrapeUTCS
 
 main = Blueprint('main', __name__)
 
 @main.route('/scrape',methods=['GET'])
 def get_scrape():
-
-        food = ['pizza','soda']
+        food = ['asdf']
+        food = scrapeUTCS()
         return jsonify({'food' : food})
+
+# from scrape import scrapeUTCS
+
+# print(scrapeUTCS())
